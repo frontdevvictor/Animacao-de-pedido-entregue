1ab2982f7fe29c7018c2f8be709dc203541d5749
Victor Hugo <frontdevvictor@gmail.com>
2024-05-31 00:08:02 -0300

    Corrijido

---
+++ script.js
@@ -0,0 +1,71 @@
+// Programador Designer Pro
+// Compartilhe com os Amigos
+$('.btn-order').click(()=> {
+    const timeline = anime.timeline({
+        easing: 'easeInSine',
+        })
+    
+        timeline.add({
+        targets: '.btn-order .default',
+        opacity: [1, 0],
+        duration: 200
+        })
+
+        timeline.add({
+        targets: '.btn-order',
+        height: ['50px', '6px'],
+        duration: 400
+        })
+    
+        timeline.add({
+        targets: '.car',
+        opacity: [0, 1]
+        }, '-=600')
+
+        timeline.add({
+        targets: '.box',
+        translateX: [0, '210px'],
+        duration: 300
+        })
+
+        timeline.add({
+        targets: '.box',
+        translateY: [0, '90px'],
+        duration: 300
+        })
+        timeline.add({
+        targets: '.light',
+        opacity: [0, 1],
+        duration: 200
+        })
+
+        timeline.add({
+        targets: '.car',
+        translateX: [0, '130px'],
+        duration: 800,
+        easing: 'easeInQuart',
+        })
+    
+        timeline.add({
+        targets: '.car',
+        opacity: [1, 0]
+        }, '-=600')
+
+        timeline.add({
+        targets: '.btn-order',
+        height: ['6px', '50px'],
+        duration: 400
+        })
+    
+        timeline.add({
+        targets: '.btn-order .completed',
+        opacity: [0, 1],
+        duration: 200
+        })
+    
+        timeline.add({
+        targets: '.completed svg',
+        strokeDashoffset: ['16px', 0],
+        duration: 300
+        })
+})
